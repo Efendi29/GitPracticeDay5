fec891aac0f8cdd49cbdacc2ac71a9be0377a039
Efendi29 <azerefem@gmail.com>
2020-10-13 19:41:41 +0200

    comment added

--- src/test/java/add_file/EU3_Practice.java
+++ src/test/java/add_file/EU3_Practice.java
@@ -3,6 +3,6 @@ package add_file;
 public class EU3_Practice {
     public static void main(String[] args) {
         System.out.println("EU3 is opening pull");
-        System.out.println("sill realy awesome hello"); // added comment
+        System.out.println("This is my own hello"); // local hello
     }
 }
