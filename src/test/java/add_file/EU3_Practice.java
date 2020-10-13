27f6d46449b827a1892600eca07d585c31fba352
Efendi29 <54336690+Efendi29@users.noreply.github.com>
2020-10-13 19:49:07 +0200

    Update EU3_Practice.java
    selam

--- src/test/java/add_file/EU3_Practice.java
+++ src/test/java/add_file/EU3_Practice.java
@@ -4,5 +4,6 @@ public class EU3_Practice {
     public static void main(String[] args) {
         System.out.println("EU3 is opening pull");
         System.out.println("sill realy awesome hello"); // added comment
+        // add some
     }
 }
