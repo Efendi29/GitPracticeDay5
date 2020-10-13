b0c80cf7e539eef91dde30679085cdc09de0be30
Efendi29 <azerefem@gmail.com>
2020-10-13 19:46:10 +0200

    adding new comment locally

--- src/test/java/add_file/EU3_Practice.java
+++ src/test/java/add_file/EU3_Practice.java
@@ -4,5 +4,6 @@ public class EU3_Practice {
     public static void main(String[] args) {
         System.out.println("EU3 is opening pull");
         System.out.println("This is my own hello"); // local hello
+        // adding some
     }
 }
