addaf550d8b15980f5ca6b78554845271c4dad09
Efendi29 <azerefem@gmail.com>
2020-10-13 19:32:11 +0200

    hello is changed

--- src/test/java/add_file/EU3_Practice.java
+++ src/test/java/add_file/EU3_Practice.java
@@ -3,5 +3,6 @@ package add_file;
 public class EU3_Practice {
     public static void main(String[] args) {
         System.out.println("EU3 is opening pull");
+        System.out.println("sill realy awesome hello");
     }
 }
